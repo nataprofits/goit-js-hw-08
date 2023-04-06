58ad53c0576bed1264c937cedb45476a1e33597e
nataprofits <nataprofits@gmail.com>
2023-04-06 16:12:46 +0100

    2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,8 +1,8 @@
-import Vimeo from '@vimeo/player';
+import Player from '@vimeo/player';
 import throttle from "lodash.throttle";
 
 const iframe = document.querySelector('iframe');
-const player = new Vimeo.Player(iframe);
+const player = new Player('vimeo-player');
 
 player.on('timeupdate', _.throttle(function() {
   localStorage.setItem('vimeo-player-current-time', player.getCurrentTime());
