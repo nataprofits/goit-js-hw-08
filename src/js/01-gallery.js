66e550f3c7ea831beb403d75af1cf094ba4fdd01
nataprofits <nataprofits@gmail.com>
2023-04-06 15:45:24 +0100

    1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,3 +3,31 @@ import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+const gallery = document.querySelector('.gallery');
+
+// Создание разметки элементов галереи из массива данных
+const galleryMarkup = galleryItems.map(({ preview, original, description }) => `
+   <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      alt="${description}"
+    />
+  </a>
+`).join('');
+
+// Вставка разметки в список ul.gallery(html)
+gallery.insertAdjacentHTML('beforeend', galleryMarkup);
+
+// Инициализация библиотеки
+let instance = new SimpleLightbox('.gallery a', { captions: true,
+  captionsData: 'alt',
+  animationSpeed: 250,
+  docClose: true,
+  overlayOpacity:0.7,
+  closeOnEscape: true, });
+  instance.show();
+;
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,17 @@
+import Vimeo from '@vimeo/player';
+import throttle from "lodash.throttle";
 
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+
+player.on('timeupdate', _.throttle(function() {
+  localStorage.setItem('vimeo-player-current-time', player.getCurrentTime());
+}, 1000));
+
+    player.getVideoTitle().then(function(title) {
+        console.log('title:', title);
+    });
+const savedTime = localStorage.getItem('vimeo-player-current-time');
+if (savedTime) {
+  player.setCurrentTime(savedTime);
+}
